36fd62810f25d5a089754e08719c4db3e3abd366
Seiji Tanaka <seiji.t@gmail.com>
2013-04-18 18:05:09 -0700

    added ability to create condition using reflection

--- Condition.cs
+++ Condition.cs
@@ -1,8 +1,10 @@
 using System;
 using UnityEngine;
+using System.Reflection;
 
 public abstract class Condition {
 	public bool target;
+	public FieldInfo info;
 	public Condition(){
 		this.target = true;
 	}
@@ -10,9 +12,14 @@ public abstract class Condition {
 		this.target = target ;
 	}
 
+	public Condition( string fieldName, bool target, Context context){
+		this.target = target;
+		info = context.GetType().GetField(fieldName);
+	}
+
 	public abstract bool Execute(Context context);
-	public bool Exist( object[] fields ){
-		foreach( object field in fields ){
+	public bool Exist( Context[] fields ){
+		foreach( Context field in fields ){
 			if( field == null ) return false;
 		}
 		return true;
